69dc8135c87ddcb817ff64124a4c41402a70ae0b
ManavShrivastava103 <165892889+ManavShrivastava103@users.noreply.github.com>
2025-10-18 13:05:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,38 +1,3 @@
-// import express from "express";
-// import mongoose from "mongoose";
-// import cors from "cors";
-// import dotenv from "dotenv";
-
-// import authRoutes from "./routes/auth.js";
-// import userRoutes from "./routes/users.js";
-// import sessionRoutes from "./routes/sessions.js";
-// import alertRoutes from "./routes/alerts.js";
-// import gymRoutes from "./routes/gyms.js";
-
-// dotenv.config();
-// const app = express();
-
-// // Middleware
-// app.use(cors());
-// app.use(express.json());
-
-// // Routes
-// app.use("/api/auth", authRoutes);
-// app.use("/api/users", userRoutes);
-// app.use("/api/sessions", sessionRoutes);
-// app.use("/api/alerts", alertRoutes);
-// app.use("/api/gyms", gymRoutes);
-
-// const PORT = process.env.PORT || 5000;
-
-// // DB Connection
-// mongoose
-//   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => {
-//     console.log("✅ MongoDB Atlas connected successfully");
-//     app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
-//   })
-//   .catch((err) => console.error(err));
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
